5478fbbb4d7b6512d39024558ce4d777914ce69e
Daniel Casey <dcasey720@gmail.com>
2019-02-13 15:38:06 +0000

    Import products app, install Pillow

--- django_soil_submit/urls.py
+++ django_soil_submit/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from accounts import urls as accounts_urls
 from home.views import index
+from products import urls as products_urls
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name='index'),
     url(r'^accounts/', include(accounts_urls)),
+    url(r'^products/', include(products_urls)),
 ]
