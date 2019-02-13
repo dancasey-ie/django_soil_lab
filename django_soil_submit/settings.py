ed762dad2f62dcba8d4aea7aefc6b779a9656fa0
Daniel Casey <dcasey720@gmail.com>
2019-02-13 15:48:05 +0000

    Sets SECRET_KEY=whatever in absence of env.py

--- django_soil_submit/settings.py
+++ django_soil_submit/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+if os.path.isfile("env.py"):
+    import env
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,8 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'whatever'
-
+# SECRET_KEY = 'whatever'
+if os.path.isfile("env.py"):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+else:
+    SECRET_KEY = 'whatever'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
