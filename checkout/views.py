34021d1e0c537f513097db762ec5eadaaaeeb79c
Daniel Casey <dcasey720@gmail.com>
2019-02-19 15:54:28 +0000

    Set username = user.username in checkout

--- checkout/views.py
+++ checkout/views.py
@@ -22,7 +22,7 @@ def checkout(request):
         if order_form.is_valid() and payment_form.is_valid():
             order = order_form.save(commit=False)
             order.date = timezone.now()
-            order.username = request.user
+            order.username = request.user.username
             order.save()
 
             cart = request.session.get('cart', {})
