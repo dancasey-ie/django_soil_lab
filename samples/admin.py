8b7c1de7f244260c2f7bdd92a2ab9993b4cb794e
Daniel Casey <dcasey720@gmail.com>
2019-02-23 13:08:38 +0000

    Include more structured sample model tabels

--- samples/admin.py
+++ samples/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Sample, Soil1Results, Soil2Results
+from .models import SampleStatus, SampleDetails, SampleResults, Sample, Soil1Results, Soil2Results
 
 admin.site.register(Sample)
 admin.site.register(Soil1Results)
 admin.site.register(Soil2Results)
+
+admin.site.register(SampleStatus)
+admin.site.register(SampleDetails)
+admin.site.register(SampleResults)
--- samples/models.py
+++ samples/models.py
@@ -6,6 +6,99 @@ from geoposition.fields import GeopositionField
 
 # Create your models here.
 
+class SampleStatus(models.Model):
+
+    STATUS_CHOICES = ((('Submitted'),('Submitted')),
+                      (('Received'),('Received')),
+                      (('Complete'),('Complete')))
+
+    sample_ref = models.CharField(max_length=50,
+                                  blank=False)
+    status = models.CharField(max_length=50,
+                              choices=STATUS_CHOICES,
+                              default='Submitted',)
+
+    submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                     on_delete=models.CASCADE,
+                                     related_name = 'submittedby',)
+    submit_date = models.DateField(auto_now_add=True)
+
+    recieved_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                    limit_choices_to={'is_staff': True},
+                                    on_delete=models.CASCADE,
+                                    related_name = 'recievedby',
+                                    blank=True,
+                                    null=True)
+    recieved_date = models.DateField(blank=True,
+                                     null=True)
+
+    tested_by = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  limit_choices_to={'is_staff': True},
+                                  on_delete=models.CASCADE,
+                                  related_name = 'testedby',
+                                  blank=True,
+                                  null=True)
+    test_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.sample_ref, self.status)
+
+class SampleDetails(models.Model):
+    SOIL_TYPES = ((('Clay - low plasticity, lean'),('Clay - low plasticity, lean')),
+                  (('Clay - high plasticity, fat'),('Clay - high plasticity, fat')),
+                  (('Clay - organic'),('Clay - organic')),
+                  (('Gravel - well-graded, fine to coarse gravel'),('Gravel - well-graded, fine to coarse gravel')),
+                  (('Gravel - poorly graded'),('Gravel - poorly graded')),
+                  (('Gravel - silty'),('Gravel - silty')),
+                  (('Gravel - clayey'),('Gravel - clayey')),
+                  (('Sand - well-graded, fine to coarse Sand'),('Sand - well-graded, fine to coarse Sand')),
+                  (('Sand - poorly graded'),('Sand - poorly graded')),
+                  (('Sand - silty'),('Sand - silty')),
+                  (('Sand - clayey'),('Sand - clayey')),
+                  (('Peat'),('Peat')),
+                  (('Silt - low plasticity, lean'),('Silt - low plasticity, lean')),
+                  (('Silt - high plasticity, fat'),('Silt - high plasticity, fat')),
+                  (('Silt - organic'),('Silt - organic')))
+
+
+    LAND_USES = ((('Beef'), ('Beef')),
+                 (('Dairy'), ('Dairy')),
+                 (('Grassland'), ('Grassland')),
+                 (('Horticulture'), ('Horticulture')),
+                 (('Sheep'), ('Sheep')),
+                 (('Tillage'), ('Tillage')))
+
+    sample = models.ForeignKey(SampleStatus, null=False)
+
+    customer_name = models.CharField(max_length=50, blank=True, null=True)
+    customer_ref_1 = models.CharField(max_length=50, blank=True, null=True)
+    customer_ref_2 = models.CharField(max_length=50, blank=True, null=True)
+    sample_location = GeopositionField(null=True)
+    sample_address = models.CharField(max_length=50, blank=True, null=True)
+    sample_date = models.DateField(blank=False, default=timezone.now)
+    soil_type = models.CharField(max_length=50, choices=SOIL_TYPES)
+    land_use = models.CharField(max_length=50, choices=LAND_USES)
+    other_comments = models.TextField(max_length=254, blank=True)
+
+
+    def __str__(self):
+        return "{0}-{1}-Details".format(self.id, self.sample.sample_ref)
+
+class SampleResults(models.Model):
+    sample = models.ForeignKey(SampleStatus, null=False)
+    p_morgan_result = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    k_morgan_result = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    lr_ph_result = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    ph_result = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    lab_comments = models.TextField(max_length=254, blank=True)
+
+    def __str__(self):
+        return "{0}-{1}-Results".format(self.id, self.sample.sample_ref)
+
+
+
+
+
 class Sample(models.Model):
     TEST_CHOICES = ((('1'),('Soil 1-SS Basic')),
                     (('2'),('Soil 2-SS Dairy/Beef/Sheep/Horses')),
