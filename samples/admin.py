797f5fe4fbd3000bd504183af64a6dc01da5ca50
Daniel Casey <dcasey720@gmail.com>
2019-02-25 13:47:23 +0000

    Remove old model references from samples/admin.py

--- samples/admin.py
+++ samples/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-from .models import SampleStatus, SampleDetails, SampleResults, Sample, Soil1Results, Soil2Results
-
-admin.site.register(Sample)
-admin.site.register(Soil1Results)
-admin.site.register(Soil2Results)
+from .models import SampleStatus, SampleDetails, SampleResults
 
 admin.site.register(SampleStatus)
 admin.site.register(SampleDetails)
