a87b1daa16115694e218667573c33eedb4756850
Daniel Casey <dcasey720@gmail.com>
2019-03-02 15:22:47 +0000

    Use geolocate to centre map

--- static/js/getLocation.js
+++ static/js/getLocation.js
@@ -1,6 +1,15 @@
-
+console.log('Tried getLocation')
 var x = document.getElementById("demo");
+var widget = document.getElementById("div_id_sample_location").getElementsByClassName("geoposition-widget")[0]
+
+var data_map_options = widget.getAttribute("data-map-option");
+var data_marker_options = widget.getAttribute("data-marker-option");
+var latInput = document.getElementById("id_sample_location_0");
+var lngInput = document.getElementById("id_sample_location_1");
+
+getLocation();
 function getLocation() {
+    console.log('Tried getLocation function')
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -9,6 +18,16 @@ function getLocation() {
 }
 
 function showPosition(position) {
+    console.log('Tried getPosition function')
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
         x.innerHTML = "Latitude: " + position.coords.latitude +
         "<br>Longitude: " + position.coords.longitude;
-    }
+    // latInput.value = lat;
+    // lngInput.value = lng;
+    data_map_options = { "zooom": 5, "center": { "lat": lat, "lng": lng }, 'mapTypeId': 'satellite'};
+    data_marker_options = {
+        'position': { 'lat': lat, 'lng': lng},
+    };
+}
+
