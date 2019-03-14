b51e02bc2de61588c02943d47723404391761567
Daniel Casey <dcasey720@gmail.com>
2019-03-14 12:50:11 +0000

    Add staticfiles directory to .gitignore

--- home/views.py
+++ home/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """A view that displays the index page"""
-    return render(request, "home.html")
+    return render(request, "home.html", {'title':'Home'})
 
 def about(request):
     """A view that displays the index page"""
-    return render(request, "about.html")
+    return render(request, "about.html", {'title':'About'})
--- samples/filter.py
+++ samples/filter.py
@@ -4,7 +4,7 @@ from django import forms
 import django_filters
 
 class SampleFilter(django_filters.FilterSet):
-   # sample_ref = django_filters.CharFilter(lookup_expr='icontains')
+    sample_ref = django_filters.CharFilter(lookup_expr='icontains')
   # sample_ref = django_filters.CharFilter(lookup_expr='icontains', widget=forms.TextInput(attrs={'class': 'form-control'}))
   # status = django_filters.ChoiceFilter(lookup_expr='exact', choices= SampleStatus.STATUS_CHOICES , widget=forms.Select(attrs={'class': 'form-control'}))
   #  ordered_by = django_filters.ChoiceFilter(lookup_expr='exact', choices=USER_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
