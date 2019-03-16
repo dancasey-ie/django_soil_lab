950a10989b363e400a492be838a3d009d9e0beb3
Daniel Casey <dcasey720@gmail.com>
2019-03-16 19:45:17 +0000

    Remove cached staticfiles

--- staticfiles/js/datePicker.7c2fd1914248.js
+++
@@ -1,8 +0,0 @@
-// script used in submitdetails.html relys on
-// <script src="https://cdnjs.cloudflare.com/ajax/libs/datepicker/0.6.5/datepicker.min.js"></script>
-
-$(function () {
-    $(".datepicker").datepicker({
-        format: 'YYYY-MM-DD'
-    });
-});
