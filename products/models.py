aeddc8b7bd8015d0bab1500d6df1ede5cd3e66c8
Daniel Casey <dcasey720@gmail.com>
2019-03-17 15:04:35 +0000

    Add more products to site

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     purpose = models.CharField(max_length=254, default='')
     elements_determined = models.CharField(max_length=254, default='')
-    description = models.TextField(max_length=254,)
+    description = models.TextField(max_length=500,)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     test_type = models.CharField(max_length=254, default='')
