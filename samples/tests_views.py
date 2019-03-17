39c33ed4e9d42448dfa7c4aa739fdd72d0620479
Daniel Casey <dcasey720@gmail.com>
2019-03-17 13:42:16 +0000

    Change home.html to index.html in tests

--- samples/tests_views.py
+++ samples/tests_views.py
@@ -8,7 +8,7 @@ class TestViews(TestCase):
     def test_get_home_page(self):
         page = self.client.get("/")
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "home.html")
+        self.assertTemplateUsed(page, "index.html")
 
     def test_products_page(self):
         page = self.client.get("/products/")
